f2ef97c62670679d8b72032f2a46491361e4fce6
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-10-16 14:51:49 +0700

    Implement more feature in backtest.py

--- backtest.py
+++ backtest.py
@@ -1,54 +1,84 @@
 from abc import ABC, abstractmethod
 import numpy as np
 import pprint
+from typing import Optional
 
 
-# class Order:
-#     def __init__(self):
-#         pass
+class Order:
+    def __init__(self, type_: str, current_price: float, i):
+        self.type = type_
+        self.open_price = current_price  # TODO: add PricePoint
+        self.open_index = i
+        self.close_price = None
+        self.close_index = None
+
+    def __repr__(self):
+        return f"Order({self.type} {self.open_price:.2f} {self.close_price:.2f})"
+
+    def pnl(self, current_price: float) -> float:  # TODO: I hate this name btw
+        return current_price - self.open_price
+
+    @property
+    def money_made(self) -> float:
+        # TODO: handle sell
+        return self.close_price - self.open_price
+
+    def close(self, current_price: float, i) -> float:
+        self.close_price = current_price
+        self.close_index = i
+        return self.money_made
+
+
+class AccountStatistics:
+    def __init__(self):
+        self.closed_orders = []
+        self.trades = 0
+        self.pnl = 0
+        # Equity curve
+        # Balance curve
+
+    def to_dict(self) -> dict:
+        return {
+            "orders": self.closed_orders,
+            "trades": self.trades,
+            "pnl": self.pnl
+        }
+
+    def update(self, order: Order):
+        self.closed_orders.append(order)
+        self.trades += 1
+        self.pnl += order.money_made
+
 
 class Account:
     def __init__(self, balance):
         self.balance = balance
-        # TODO: make Order class
-        self.order = None  # (just the open price for now)
+        self.order: Optional[Order] = None 
 
-        # primary stats
-        # TODO: combine stats
-        # self.stats = {
-        #     "closed_orders": {},
-        #     "trades": 0,
-        #     "pnl": 0,
-        # }
-        self.closed_orders = {}  # time: price
-        self.trades = 0
-        self.pnl = 0
+        self.stats = AccountStatistics()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Account({self.balance}, {self.order})"
 
-    def equity(self, price):
+    def equity(self, current_price: float):
         if not self.order:
             return self.balance
-        return self.balance + (self.order - price)  # TODO: use order.pnl()
+        return self.balance + self.order.pnl(current_price)
 
     # --- interfaces for Strategy
-    def buy(self, price):
+    def buy(self, current_price: float, i):
         if self.order is None:
-            self.order = price  # TODO
+            self.order = Order("buy", current_price, i)
 
-    # TODO: implement sell
+    def sell(self, current_price: float):
+        raise NotImplementedError
 
-    def close(self, price, i):
+    def close(self, current_price: float, i):
         if self.order:
-            # pnl = self.order.close(price) #TODO set order flag as closed
-            pnl = price - self.order
+            pnl = self.order.close(current_price, i)
             self.balance += pnl
+            self.stats.update(self.order)
             self.order = None
-
-            self.trades += 1  # TODO: stats
-            self.pnl += pnl
-            self.closed_orders[i] = price
 
 
 class Strategy(ABC):
@@ -56,43 +86,37 @@ class Strategy(ABC):
     def act(self, account, prices: np.array):  # hopefully view won't slow down hot loop
         pass
 
+
 class RandomAction(Strategy):
     def act(self, account: Account, prices: np.array, i):
         if not account.order:
             if np.random.random() < 0.5:
-                account.buy(prices[-1])  # clearly need a np.array wrapper that expose current price, maybe a tiny pd.DataFrame design?
+                account.buy(prices[-1], i)  # clearly need a np.array wrapper that expose current price, maybe a tiny pd.DataFrame design?
         else:
             if np.random.random() < 0.1:
                 account.close(prices[-1], i)
     
 
 class Simulation:
-    def __init__(self, data: np.array, strategy: Strategy):
+    def __init__(self, data: np.array, strategy: Strategy, account: Account):
         self.data = data  # TODO: make data good, support index and time
         self.strategy = strategy
         self.summary = None
+        self.account = account
 
     def run(self):
-        account = Account(100)
-
         for i in range(1, len(self.data)):
-            if account.equity(self.data[i]) <= 0:
+            if self.account.equity(self.data[i]) <= 0:
                 break
-            print(i, account)  # TODO: improve logging
-            self.strategy.act(account, self.data[:i], i)
+            self.strategy.act(self.account, self.data[:i], i)
 
-        if account.order:
-            account.close(self.data[-1], len(self.data))
+        if self.account.order:
+            self.account.close(self.data[-1], i)
 
         # using dict is ok, every ML libs do it.
         # represent ground truth, derive meaningful ratios and features separately
         # or maybe just put common ratios in? maybe no
-        self.summary = {
-            "balance": account.balance,
-            "trades": account.trades,
-            "pnl": account.pnl,
-            "orders": account.closed_orders.items()
-        } 
+        self.summary = {"account": self.account.stats.to_dict()} 
 
 """
 # sklearn
@@ -114,19 +138,19 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     data = np.full(100, 42) + np.random.normal(0.1, 1, 100).cumsum()
-
     strategy = RandomAction()
+    account = Account(100)
 
-    sim = Simulation(data, strategy)
+    sim = Simulation(data, strategy, account)
     sim.run()
     pprint.pprint(sim.summary)
 
     sum = sim.summary
-    orders = dict(sum["orders"])
-    # print(order)
+    orders: list[Order] = sum["account"]["orders"]
     plt.title("Backtest Result")
     plt.xlabel("Index")
     plt.ylabel("Price")
     plt.plot(data)
-    plt.scatter(orders.keys(), orders.values(), marker="^", color="green")
+    for o in orders:
+        plt.plot([o.open_index, o.close_index], [o.open_price, o.close_price], color="green", lw=2, ls="--", ms=5, marker="o",)
     plt.show()
