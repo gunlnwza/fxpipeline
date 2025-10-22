8e063535ed411e1da16514c595d117e94ea24e10
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-10-22 20:34:05 +0700

    Format ForexPriceRequest string

--- fxpipeline/data/core.py
+++ fxpipeline/data/core.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import datetime
 
 import pandas as pd
 
@@ -8,8 +9,13 @@ from currencies import CurrencyPair
 @dataclass
 class ForexPriceRequest:
     pair: CurrencyPair
-    start: str
-    end: str
+    start: datetime.datetime
+    end: datetime.datetime
+
+    def __str__(self) -> str:
+        start = self.start.strftime("%Y-%m-%d %H:%M:%S")
+        end = self.end.strftime("%Y-%m-%d %H:%M:%S")
+        return f"{self.pair}[{start}, {end}]"
 
 
 @dataclass
