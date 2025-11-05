137396fdc433d40303a32ad43c9b851ff30e0626
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-11-05 16:48:27 +0700

    Rendering ok

--- trading_env.py
+++ trading_env.py
@@ -20,11 +20,10 @@ class TradingEnv(gym.Env):
         # pygame
         self.render_mode = render_mode
         if render_mode == "human":
-            print('set up screen')
             pg.display.init()
             self.screen = pg.display.set_mode((500, 500))
             self.clock = pg.time.Clock()
-            self.fps = 20
+            self.fps = 5
 
         # data
         self._df = df
@@ -58,14 +57,15 @@ class TradingEnv(gym.Env):
             pnl = -(cur_price - prev_price)
         reward = pnl
 
+        info = {}
         done = self.render()
         if done:
-            return obs, reward, done, False, {}
+            return obs, reward, done, False, info
 
         # advance time
         self._i += 1
         done = self._i >= len(self._closes)
-        return obs, reward, done, False, {}
+        return obs, reward, done, False, info
 
     def reset(self, seed=None):
         """Reset environment to initial state"""
@@ -74,18 +74,31 @@ class TradingEnv(gym.Env):
 
     def render(self):
         """Visualize what the agent sees"""
+        obs = self._get_obs()
         if self.render_mode == "terminal":
-            obs = self._get_obs()
-            print(f"i = {self._i}: {obs}")
+            print(f"i = {self._i}")
         elif self.render_mode == "human":
             for event in pg.event.get():
-                if event.type == pg.QUIT:
+                if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_q):
                     self.close()
                     self.render_mode = None
                     return True
 
+            w = self.screen.get_width()
+            h = self.screen.get_height()
+
+            def to_screen_x(x):
+                return 10 + (w // (len(obs) + 1)) * x
+
+            def to_screen_y(x):
+                return h // 2 - x * h // 10
+
             self.screen.fill("black")
-            pg.draw.circle(self.screen, "white", (self._i % 500, self._i % 500), 50)
+            for i in range(len(obs) - 1):
+                start = (to_screen_x(i), to_screen_y(obs[i]))
+                end = (to_screen_x(i + 1), to_screen_y(obs[i + 1]))
+                pg.draw.line(self.screen, "white", start, end, 1)
+
             pg.display.flip()
             self.clock.tick(self.fps)
 
@@ -110,12 +123,11 @@ def main():
     handle_sigint()
 
     df = load_forex_price("EURUSD")
-    env = TradingEnv(df, obs_size=10, render_mode="human")
+    env = TradingEnv(df, obs_size=50, render_mode="human")
     model = DummyModel()
 
     obs, _ = env.reset()
-    for _ in range(100):
-        print(_)
+    while True:
         action, states = model.predict(obs)
         obs, reward, terminated, truncated, _ = env.step(action)
         if terminated or truncated:
