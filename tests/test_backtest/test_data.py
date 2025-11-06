9840224a1bd793fe2ffb36c4fe5e9dee6e9f2391
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-11-06 20:52:54 +0700

    WIP: trying to figure out the best way to test env

---
+++ tests/test_backtest/test_data.py
@@ -0,0 +1,48 @@
+import pytest
+
+
+# general
+def test_get_observation():
+    pass
+
+
+def test_get_info():
+    pass
+
+
+def test_step():
+    pass
+
+
+def test_reset():
+    pass
+
+
+def test_is_terminated():
+    pass
+
+
+def test_is_truncated():
+    pass
+
+
+# states
+def test_current_price():
+    pass
+
+
+# Order
+def test_buy():
+    pass
+
+
+def test_sell():
+    pass
+
+
+def test_close_order():
+    pass
+
+
+def test_current_profit():
+    pass
---
+++ tests/test_backtest/test_trading_env.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from gymnasium import spaces
+
+from fxpipeline.backtest import TradingEnv
+
+"""
+NOTE: does not test rendering-related features
+"""
+
+rng = np.random.default_rng(seed=42)
+
+df = pd.DataFrame({
+    "close": rng.random(10)
+
+})
+env = TradingEnv(df, obs_size=5, render_mode=None)
+
+def test_action_space():
+    assert env.action_space == spaces.Discrete(3, start=-1)
+
+def test_observation_space():
+    """Must infer shape from passed df"""
+    pass
+
+def test_get_observation():
+    pass
+
+def test_get_info():
+    pass
+
+
+def test_step():
+    pass
+
+
+def test_reset():
+    pass
