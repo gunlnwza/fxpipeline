3db6607c321c393fdd129c7e4641d877127e09c2
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-11-16 00:35:17 +0700

    Close db connection, adjust behavior

--- fxpipeline/ingestion/get.py
+++ fxpipeline/ingestion/get.py
@@ -10,8 +10,8 @@ from .database import SQLiteDatabase
 from ..core import ForexPrice, make_pair, CurrencyPair
 
 load_dotenv()
+CACHES_PATH = os.getenv("CACHES_PATH")
 logger = logging.getLogger(__file__)
-db = SQLiteDatabase(f"{os.getenv("CACHES_PATH")}/prices.db")
 
 
 def _convert_pairs(pairs: str | CurrencyPair | list[str | CurrencyPair]) -> list[CurrencyPair]:
@@ -21,11 +21,11 @@ def _convert_pairs(pairs: str | CurrencyPair | list[str | CurrencyPair]) -> list
         return [make_pair(p) for p in pairs]
 
 
-def _convert_start_end(start, end):
+def _convert_start_end(start, end, days):
     if end is None:
         end = pd.Timestamp.now()
     if start is None:
-        start = end - pd.Timedelta(days=30)
+        start = end - pd.Timedelta(days=days)
     start = pd.Timestamp(start)
     end = pd.Timestamp(end)
     return start, end
@@ -34,13 +34,16 @@ def _convert_start_end(start, end):
 def fetch_forex_prices(pairs: str | CurrencyPair | list[str | CurrencyPair], source: str,
                        start: str | pd.Timestamp | None = None,
                        end: str | pd.Timestamp | None = None) -> list[ForexPrice]:
+    print(f"📦 Fetching with {source.upper()} API")
+    sw_0 = Stopwatch()
+
     pairs = _convert_pairs(pairs)
-    start, end = _convert_start_end(start, end)
+    start, end = _convert_start_end(start, end, 30)
+
+    db = SQLiteDatabase(f"{CACHES_PATH}/prices.db")
     loader = get_loader(source)
     res = []
 
-    print(f"📦 Fetching with {source.upper()} API")
-    sw_0 = Stopwatch()
     for pair in pairs:
         print(f"📈 {pair} [{source}]... ", end="", flush=True)
         if db.have(pair, source, start, end):
@@ -54,6 +57,7 @@ def fetch_forex_prices(pairs: str | CurrencyPair | list[str | CurrencyPair], sou
             print(f"⏱️  {sw_1.time:.2f}s")
         res.append(data)
 
+    db.close()
     print(f"✔️  All done in {sw_0.time:.2f}s")
     return res
 
@@ -62,7 +66,9 @@ def load_forex_prices(pairs: str | CurrencyPair | list[str | CurrencyPair], sour
                       start: str | pd.Timestamp | None = None,
                       end: str | pd.Timestamp | None = None) -> list[ForexPrice]:
     pairs = _convert_pairs(pairs)
-    start, end = _convert_start_end(start, end)
+    start, end = _convert_start_end(start, end, 10000)
 
+    db = SQLiteDatabase(f"{CACHES_PATH}/prices.db")
     res = [db.load(pair, source, start, end) for pair in pairs]
+    db.close()
     return res
