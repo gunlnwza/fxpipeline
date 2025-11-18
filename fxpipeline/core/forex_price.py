1e7061988fd36d6c147e79b9f0722da1085a61d5
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-11-18 20:21:58 +0700

    Add ticker property for ForexPrice

--- fxpipeline/core/forex_price.py
+++ fxpipeline/core/forex_price.py
@@ -11,5 +11,9 @@ class ForexPrice:
     source: str
     df: pd.DataFrame
 
+    @property
+    def ticker(self):
+        return self.pair.ticker
+
     def copy(self):
         return ForexPrice(self.pair, self.source, self.df.copy())
