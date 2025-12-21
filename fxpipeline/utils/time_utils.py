3ff8d1acefeb1d85bffa1aa806ac78e3dca4f815
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-12-21 16:24:01 +0700

    core: add util functions

---
+++ fxpipeline/utils/time_utils.py
@@ -0,0 +1,26 @@
+import time
+
+
+class Stopwatch:
+    def __init__(self):
+        self.start()
+
+    def start(self):
+        self._start = time.perf_counter()
+        self._stop = None
+
+    def stop(self):
+        self._stop = time.perf_counter()
+
+    @property
+    def time(self):
+        if self._stop:
+            return self._stop - self._start
+        else:
+            return time.perf_counter() - self._start
+
+    def __repr__(self):
+        return f"Stopwatch({self.time:.3f}s)"
+
+    def __str__(self):
+        return f"{self.time:.3f}s"
