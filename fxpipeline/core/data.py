bce1096ef638dd85551ea7236f6a483c6a0f52b1
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-12-22 10:09:00 +0700

    core: edit Data

--- fxpipeline/core/data.py
+++ fxpipeline/core/data.py
@@ -6,11 +6,9 @@ import pandas as pd
 from .price import ForexPrice
 
 
-# TODO: think about access mechanism for when data is of different length, (use timestamp)
 @dataclass
 class Data:
     price: ForexPrice
-    economics: Optional[pd.DataFrame]
 
     def __len__(self) -> int:
         return len(self.price)
