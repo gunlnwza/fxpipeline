16494beffc67e92b5a70df41a2821620ff442222
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-12-22 21:21:47 +0700

    wip: save test_backtest.py

--- test_backtest.py
+++ test_backtest.py
@@ -1,10 +1,15 @@
 from dotenv import load_dotenv
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import mplfinance as mpf
 
-from fxpipeline.core import Data
+from fxpipeline.core import Data, ForexPrices, CurrencyPair
 from fxpipeline.ingestion import load_forex_prices
 from fxpipeline.backtest import Backtester, Strategy
 from fxpipeline.utils import Stopwatch
+
+import random
 
 
 if __name__ == "__main__":
@@ -22,9 +27,42 @@ if __name__ == "__main__":
     #     [9.5, 9.9, 9.1, 9.7, 0],
     #     [10.5, 10.9, 10.1, 10.7, 0]
     # ])
-    # prices = ForexPrices(CurrencyPair("ABC", "DEF", 0.0001), "mock", df)
 
-    prices = load_forex_prices("USDJPY", source="alpha_vantage")
+    # n = 5000
+    # rng = np.random.default_rng(42)
+
+    # closes = 100 + rng.normal(0, 0.01, n).cumsum()
+
+    # opens = np.empty(n)
+    # opens[0] = closes[0]
+    # opens[1:] = closes[:-1] + rng.normal(0, 0.005, n - 1)
+
+    # body_high = np.maximum(opens, closes)
+    # body_low  = np.minimum(opens, closes)
+
+    # wick_up = rng.exponential(0.004, n)
+    # wick_dn = rng.exponential(0.004, n)
+
+    # highs = body_high + wick_up
+    # lows  = body_low - wick_dn
+
+    # df = pd.DataFrame({
+    #     "Open": opens,
+    #     "High": highs,
+    #     "Low": lows,
+    #     "Close": closes,
+    #     "Volume": 0
+    # }, index=pd.date_range(start='2000-01-01', periods=n, freq='D'))
+
+    # mpf.plot(df, 
+    #     type='candle', 
+    #     ylabel='Price ($)', 
+    #     style='yahoo',
+    #     volume=True
+    # )
+
+    # prices = ForexPrices(CurrencyPair("ABC", "DEF", 0.0001), "mock", df)
+    prices = load_forex_prices("GBPUSD", source="alpha_vantage")
 
     data = Data(prices)
     strategy = Strategy()
